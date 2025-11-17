f86278727a88efad38d72e5bcce13637fac7f4ae
Jakub Mikołajczyk <kubamiko11@gmail.com>
2025-11-17 21:36:40 +0100

    remove returning computing past attribute in event serializer

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -7,8 +7,4 @@ class EventSerializer < BaseSerializer
   has_many :tickets, serializer: TicketSerializer
 
   attributes :id, :name, :description, :place, :date, :category, :created_at, :updated_at
-
-  attribute :past do |object|
-    object.past?
-  end
 end
